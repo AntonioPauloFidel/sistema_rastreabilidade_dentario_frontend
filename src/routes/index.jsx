a7ba7fe7b0b3fa720d1793b86ae329b8ca23ef77
Antonio Paulo Pereira Fidel <antoniopfdl.fidel@gmail.com>
2026-06-11 20:37:17 -0300

    feat: criar layouts LayoutAutenticado e LayoutPublico com Breadcrumb
    closes #30

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
 import { Loading } from '../components/Loading'
+import { LayoutAutenticado } from '../layouts/LayoutAutenticado'
+import { LayoutPublico } from '../layouts/LayoutPublico'
 import NaoEncontrado from '../pages/NaoEncontrado'
 
 function ProtectedRoute() {
@@ -28,27 +30,31 @@ export function AppRoutes() {
       <Routes>
         {/* Rotas públicas */}
         <Route element={<PublicRoute />}>
-          <Route path="/login" element={<div>Login</div>} />
-          <Route path="/consulta" element={<div>Consulta pública</div>} />
+          <Route element={<LayoutPublico />}>
+            <Route path="/login" element={<div>Login</div>} />
+            <Route path="/consulta" element={<div>Consulta pública</div>} />
+          </Route>
         </Route>
 
         {/* Rotas protegidas */}
         <Route element={<ProtectedRoute />}>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/dashboard" element={<div>Dashboard</div>} />
-          <Route path="/dentes" element={<div>Dentes</div>} />
-          <Route path="/dentes/:id" element={<div>Detalhe do dente</div>} />
-          <Route path="/doadores" element={<div>Doadores</div>} />
-          <Route path="/remessas" element={<div>Remessas</div>} />
-          <Route path="/solicitacoes" element={<div>Solicitações</div>} />
-          <Route path="/cessoes" element={<div>Cessões</div>} />
-          <Route path="/instituicoes" element={<div>Instituições</div>} />
-          <Route path="/clinicas" element={<div>Clínicas</div>} />
-          <Route path="/dentistas" element={<div>Dentistas</div>} />
-          <Route path="/locais" element={<div>Locais</div>} />
-          <Route path="/usuarios" element={<div>Usuários</div>} />
-          <Route path="/auditoria" element={<div>Auditoria</div>} />
-          <Route path="/perfil" element={<div>Perfil</div>} />
+          <Route element={<LayoutAutenticado />}>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/dashboard" element={<div>Dashboard</div>} />
+            <Route path="/dentes" element={<div>Dentes</div>} />
+            <Route path="/dentes/:id" element={<div>Detalhe do dente</div>} />
+            <Route path="/doadores" element={<div>Doadores</div>} />
+            <Route path="/remessas" element={<div>Remessas</div>} />
+            <Route path="/solicitacoes" element={<div>Solicitações</div>} />
+            <Route path="/cessoes" element={<div>Cessões</div>} />
+            <Route path="/instituicoes" element={<div>Instituições</div>} />
+            <Route path="/clinicas" element={<div>Clínicas</div>} />
+            <Route path="/dentistas" element={<div>Dentistas</div>} />
+            <Route path="/locais" element={<div>Locais</div>} />
+            <Route path="/usuarios" element={<div>Usuários</div>} />
+            <Route path="/auditoria" element={<div>Auditoria</div>} />
+            <Route path="/perfil" element={<div>Perfil</div>} />
+          </Route>
         </Route>
 
         <Route path="*" element={<NaoEncontrado />} />
