c285dea8cf3c60e0d4c0fb7872d098af581791dc
Antonio Paulo Pereira Fidel <antoniopfdl.fidel@gmail.com>
2026-06-11 20:27:22 -0300

    feat: criar cliente HTTP com axios e servicos por modulo
    closes #3

---
+++ src/services/locais.service.js
@@ -0,0 +1,8 @@
+import { api } from './api'
+
+export const locaisService = {
+  listar: (params) => api.get('/api/locais', { params }),
+  buscarPorId: (id) => api.get(`/api/locais/${id}`),
+  criar: (dados) => api.post('/api/locais', dados),
+  atualizar: (id, dados) => api.put(`/api/locais/${id}`, dados),
+}
