cc2647f602f9134db68fb5e31eeeeeb31b7645a4
Antonio Paulo Pereira Fidel <antoniopfdl.fidel@gmail.com>
2026-06-11 20:01:03 -0300

    feat: criar pagina 404 e Error Boundary para erros inesperados
    closes #31

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NaoEncontrado from '../pages/NaoEncontrado'
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>Home do SIRDE</div>} />
+        <Route path="*" element={<NaoEncontrado />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
